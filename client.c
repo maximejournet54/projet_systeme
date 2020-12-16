efb9c6018a9361b265e601e6f0d8585a7872bbb9
maximejournet54 <maxime.journet@free.fr>
2020-12-16 09:39:37 +0100

    ajout sujet + fichiers

---
+++ client.c
@@ -0,0 +1,25 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+//il y a peut etre pas besoin de tous les import
+
+
+//entête des fonctions, a mettre dans un fichier .h a la fin
+
+
+
+int main(int argc , char const *argv[]) {
+
+
+}
+
+
+//corps des fonctions
