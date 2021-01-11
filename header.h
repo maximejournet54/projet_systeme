465b21554fdb647399bffadc2bf64671464422d0
LucasLvs17 <46243866+LucasLvs17@users.noreply.github.com>
2021-01-11 22:12:57 +0100

    idTicket[0] -> idTicket[20], err inser fichier

--- header.h
+++ header.h
@@ -7,7 +7,7 @@ void genererNumeroDossier(char buffer[]);
 typedef struct t Ticket;
 
 struct Ticket{
-    char idTicket[0];//char au lieu de int car fprintf attend comme argument un char*
+    char idTicket[20];//char au lieu de int car fprintf attend comme argument un char*
     char nom[100];
     char prenom[100];
 } t;
